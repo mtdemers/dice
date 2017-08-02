65dce7c2951f7eeb48c6bdbdbbf6bfa8088246ea
mtdemers <mtdemers@github.com>
2017-08-02 12:45:42 -0700

    Working

--- dice.py
+++ dice.py
@@ -1,31 +1,80 @@
 ###Dice roller
+###random int generator
+from random import randint 
 
-print "How many dice would you like to roll?"
+###Get number of dice, assign global variable
+def dice_input():
+    global dice
+    dice = int(raw_input('How many dice would you like to roll? ')) #input for dice number variable
+    dice_check()
+    return
 
-dice = raw_input()
-dice = int(dice)
+###Get number of sides, assign global variable
+def sides_input():
+    global sides
+    sides = int(raw_input('How many sides does each dice have? '))
+    sides_check()
+    return
 
-if dice <= 0:
-    print "Error! Please enter a positive number of dice."
-    elif type(dice) == str:
-        print "Error! Please enter a positive number of dice."
+###Loop through number of dice, print random number per sides
+def roll_dice():
+    print "Ok, rolling %s dice with %s sides." % (dice, sides)
+    roll = 0
+    for roll in range(0, dice):
+        print("[%s]" % (randint(1,sides)))
+
+def dice_check(): ###Check for positive int
+    dice_int = False
+    while not dice_int: 
+        if dice <= 0: 
+            print "Error! Please enter a positive number of dice."
+            dice_input()
+        else:
+            dice_int = True
+            print "Ok, I will roll %s dice." % (dice)
+
+def sides_check(): ###Check for positive int
+    sides_int = False
+    while not sides_int: 
+        if sides <= 0: 
+            print "Error! Please enter a positive number of sides."
+            sides_input()
+        else:
+            sides_int = True
+
+###check to see if user would like to roll again
+def reroll(): 
+    again = raw_input('Would you like to reroll, make a change, or end r/c/e? ')
+    if again == 'r':
+        roll_dice()
     else:
-        print "Ok, I will roll %s dice." % (dice)
+        if again == 'e':
+            print "Ok, thanks!"
+            return
+        else:
+            if again == 'c':
+                change()
+            else:
+                print "Please respond with y or n"
+                reroll()
+    reroll()
 
-print "How many sides should each dice have?"
-
-sides = raw_input()
-sides = int(sides)
-
-if sides <= 0:
-    print "Error! Please enter a positive number of sides."
-    elif type(sides) == str:
-        print "Error! Please enter a positive number of sides."
+###Change number of dice or sides
+def change():
+    which = raw_input('Change dice or sides d/s? ')
+    if which == 'd':
+        dice_input()
+        roll_dice()
     else:
-        print "Ok, rolling %s sided dice." % (sides)
+        if which == 's':
+            sides_input()
+            roll_dice()
+        else:
+            print 'Please input only d or s'
+            change()
 
-roll = 0
 
-for roll in range(1, dice):
-    roll = random.randint(1, sides)
-    print roll
+dice_input() ###Get number of dice
+sides_input() ###Get number of sides
+roll_dice()
+reroll()
