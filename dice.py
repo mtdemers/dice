2f114e255c0381516e7c5ef81649c7152355c796
mtdemers <mtdemers@github.com>
2017-08-03 10:10:57 -0700

    pep8 changes, remove globals

--- dice.py
+++ dice.py
@@ -1,31 +1,32 @@
-# Dice roller
+"""Dice roller - rolls any number of dice with any number of sides"""
 # random int generator
 from random import randint
 
+
 def main():
+    """main function"""
     pass
 
 if __name__ == "__main__":
     main()
 
+
 def dice_input():
     """Get the number of dice."""
-    global dice
     # input for dice number variable
     dice = int(raw_input('How many dice would you like to roll? '))
-    dice_check()
-    return
+    dice_check(dice)
+    return dice
 
 
 def sides_input():
     """Get the number of sides"""
-    global sides
     sides = int(raw_input('How many sides does each dice have? '))
-    sides_check()
-    return
+    sides_check(sides)
+    return sides
 
 
-def roll_dice():
+def roll_dice(dice, sides):
     """Loop through number of dice, randomly generate numbers"""
     print "Ok, rolling %s dice with %s sides." % (dice, sides)
     roll = 0
@@ -33,7 +34,7 @@ def roll_dice():
         print "[%s]" % (randint(1, sides))
 
 
-def dice_check():
+def dice_check(dice):
     """Check for positive integers"""
     dice_int = False
     while not dice_int:
@@ -45,7 +46,7 @@ def dice_check():
             print "Ok, I will roll %s dice." % (dice)
 
 
-def sides_check():
+def sides_check(sides):
     """Check for positive integers"""
     sides_int = False
     while not sides_int:
@@ -56,7 +57,7 @@ def sides_check():
             sides_int = True
 
 
-def reroll():
+def reroll(dice, sides):
     """Ask user for reroll"""
     again = raw_input('Would you like to reroll, make a change, or end r/c/e? ')
     if again == 'r':
@@ -65,14 +66,14 @@ def reroll():
         print "Ok, thanks!"
         return
     elif again == 'c':
-        change()
+        change(dice, sides)
     else:
         print "Please respond with r, c, or e"
         reroll()
     reroll()
 
 
-def change():
+def change(dice, sides):
     """Change number of dice or sides"""
     which = raw_input('Change dice or sides d/s? ')
     if which == 'd':
@@ -89,5 +90,5 @@ def change():
 
 dice_input()
 sides_input()
-roll_dice()
-reroll()
+roll_dice(dice, sides)
+reroll(dice, sides)
